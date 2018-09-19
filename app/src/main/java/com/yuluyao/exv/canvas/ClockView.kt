4ef53a7de3bf9351abb2b5ec710d44616c746a3a
yuluyao <wushenggoo@gmail.com>
2018-09-19 14:10:55 +0800

    save

--- app/src/main/java/com/yuluyao/exv/canvas/ClockView.kt
+++ app/src/main/java/com/yuluyao/exv/canvas/ClockView.kt
@@ -4,16 +4,14 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
 class ClockView : View {
-  constructor(context: Context) : super(context)
-  constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+  constructor( context: Context) : super(context)
+  constructor( context: Context, attrs: AttributeSet) : super(context, attrs)
 
   private val paint = Paint()
-  private val path = Path()
 
   init {
     paint.isAntiAlias = true
@@ -22,10 +20,6 @@ class ClockView : View {
     paint.strokeWidth = 2f
 
     paint.textSize = 45f
-
-  }
-
-  private fun applyAttrs(context: Context, attrs: AttributeSet) {
 
   }
 
@@ -47,14 +41,6 @@ class ClockView : View {
   private fun drawClock(canvas: Canvas) {
     canvas.translate(mWidth / 2, mHeight / 2)
     canvas.drawCircle(0f, 0f, 400f, paint)
-//    canvas.drawCircle(0f, 0f, 380f, paint)
-//    canvas.drawCircle(0f, 0f, 390f, paint)
-
-//    for (i in 0 until 360 step 10) {
-//      canvas.rotate(i.toFloat())
-//      canvas.drawLine(380f, 0f, 400f, 0f, paint)
-//    }
-    path.addCircle(mWidth / 2, mHeight / 2, 420f, Path.Direction.CW)
 
     canvas.save()
     canvas.rotate(-90f)
@@ -66,7 +52,6 @@ class ClockView : View {
       if (degrees % 30f == 0f) {
         canvas.drawLine(380f, 0f, 400f, 0f, paint)
 //        canvas.drawText("${(degrees / 30).toInt()}", 420f, 0f, paint)
-        canvas.drawTextOnPath("helloworld11",path,0f,0f,paint)
       } else {
         canvas.drawLine(390f, 0f, 400f, 0f, paint)
       }
